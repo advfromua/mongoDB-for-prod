2f365e4f0776027ab74fcd106adadde817e9e6ea
Vladyslav <advfromua@gmail.com>
2026-03-24 00:11:48 +0100

    Mongo Play

--- data/database.js
+++ data/database.js
@@ -13,6 +13,8 @@ console.log('Trying to connect to db');
 try {
   await client.connect();
   await client.db(dbName).command({ ping: 1 });
+  let x = await client.db('sample_mflix').command({ find: 'db.users;' });
+  console.log(Outcome from DB: ${x});
   console.log('Connected successfully to server');
 } catch (error) {
   console.log('Connection failed.');
